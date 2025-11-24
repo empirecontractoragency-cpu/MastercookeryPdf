b938d06349195b149d2c8d5dde84aea826537c8c
empirecontractoragency-cpu <empirecontractoragency@gmail.com>
2025-11-24 02:46:45 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    base: './', // Ensures relative paths for assets, making it easier to deploy on any static host
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-    }
-});
